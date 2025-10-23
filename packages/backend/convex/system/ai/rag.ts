f825a6e8aa10253099e0a41cac77199974e8326c
felipe <felipe.garciar@upb.edu.co>
2025-10-23 18:43:32 -0500

    20: generating embeddings

---
+++ packages/backend/convex/system/ai/rag.ts
@@ -0,0 +1,16 @@
+import { google } from "@ai-sdk/google";
+import { RAG } from "@convex-dev/rag";
+import { components } from "../../_generated/api";
+
+const googleModel = google.textEmbeddingModel("text-embedding-004");
+
+const rag = new RAG(components.rag, {
+    textEmbeddingModel: {
+        ...googleModel,
+        specificationVersion: "v2",
+        doEmbed: googleModel.doEmbed,
+    } as any,
+    embeddingDimension: 768,
+})
+
+export default rag;
